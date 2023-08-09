53151b6970471aad284e08293c7bf677e8a0556e
User Name Test <spirit2spiiirit9spirittt@gmail.com>
2023-08-09 17:21:03 +0300

    implementing Google login.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { GoogleAuthProvider } from '@angular/fire/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private db: AngularFireDatabase
+  ) {}
 
+  login() {
+    this.afAuth.signInWithRedirect(new GoogleAuthProvider());
+  }
 }
