11543ef8a48e52f33a60460baa1ab9ad712369a4
User Name Test <spirit2spiiirit9spirittt@gmail.com>
2023-08-10 13:19:27 +0300

    Implement the logout and display the current user

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css'],
 })
-export class BsNavbarComponent {}
+export class BsNavbarComponent {
+  user$: Observable<any>;
+
+  constructor(private afAuth: AngularFireAuth) {
+    this.user$ = afAuth.authState;
+  }
+
+  logout() {
+    this.afAuth.signOut();
+  }
+}
