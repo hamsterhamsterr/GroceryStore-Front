3c17d96516a615d160af77ef15c1c35f7b7f133d
User Name Test <spirit2spiiirit9spirittt@gmail.com>
2023-08-14 13:01:28 +0300

    Display the list of products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ProductService } from 'src/app/product.service';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
-  styleUrls: ['./admin-products.component.css']
+  styleUrls: ['./admin-products.component.css'],
 })
 export class AdminProductsComponent {
+  products$;
 
+  constructor(private productService: ProductService) {
+    this.products$ = this.productService.getAll();
+  }
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +9,16 @@ export class ProductService {
   constructor(private db: AngularFireDatabase) {}
 
   create(product: any) {
-    return this.db.list('/products').push(product);
+    let refToCreatedProduct = this.db.list('/products').push(product);
+    let idOfCreatedProduct = refToCreatedProduct.key;
+    this.db
+      .object('/products/' + idOfCreatedProduct)
+      .update({ id: idOfCreatedProduct });
+
+    return refToCreatedProduct;
+  }
+
+  getAll(): Observable<any> {
+    return this.db.list('/products').valueChanges();
   }
 }
