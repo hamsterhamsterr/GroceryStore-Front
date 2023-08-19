1f805e163a118f6984478e959336672141c9a7c8
User Name Test <spirit2spiiirit9spirittt@gmail.com>
2023-08-19 14:43:25 +0300

    Build a simple shopping cart page.

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -3,6 +3,10 @@ import { ShoppingCartItem } from './shopping-cart-item';
 export class ShoppingCart {
   constructor(public items: ShoppingCartItem[]) {}
 
+  get productIds(): any {
+    return Object.keys(this.items);
+  }
+
   get totalItemsCount() {
     let count = 0;
 
