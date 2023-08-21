bfa578c2069605f1819d1d2033aa2a8e38fdfd33
User Name Test <spirit2spiiirit9spirittt@gmail.com>
2023-08-21 12:47:26 +0300

    Clearing the shopping cart.

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { ShoppingCartService } from './shopping-cart.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrderService {
-  constructor(private db: AngularFireDatabase) {}
+  constructor(
+    private db: AngularFireDatabase,
+    private shoppingCartService: ShoppingCartService
+  ) {}
 
-  storeOrder(order: any) {
-    return this.db.list('/orders').push(order);
+  async placeOrder(order: any) {
+    let result = await this.db.list('/orders').push(order);
+    this.shoppingCartService.clearCart();
+    return result;
   }
 }
