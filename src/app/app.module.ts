f2bc5f66c2069b5bfcc38fd7621fdd991618a316
User Name Test <spirit2spiiirit9spirittt@gmail.com>
2023-08-22 13:11:04 +0300

    Create the core module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,9 @@ import { environment } from 'src/environments/environment.development';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { BsNavbarComponent } from './core/components/bs-navbar/bs-navbar.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './core/components/home/home.component';
 import { ProductsComponent } from 'shopping/components/products/products.component';
 import { ShoppingCartComponent } from './shopping/components/shopping-cart/shopping-cart.component';
 import { CheckOutComponent } from './shopping/components/check-out/check-out.component';
@@ -17,7 +17,7 @@ import { OrderSuccessComponent } from './shopping/components/order-success/order
 import { MyOrdersComponent } from './shopping/components/my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/components/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/components/admin-orders/admin-orders.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './core/components/login/login.component';
 import { AdminAuthGuard as AdminAuthGuard } from './admin/services/admin-auth-guard.service';
 import { ProductFormComponent } from './admin/components/product-form/product-form.component';
 import { FormsModule } from '@angular/forms';
@@ -31,19 +31,16 @@ import { ShippingFormComponent } from './shopping/components/shipping-form/shipp
 import { SharedModule } from 'shared/shared.module';
 import { AdminModule } from 'admin/admin.module';
 import { ShoppingModule } from 'shopping/shopping.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BsNavbarComponent,
-    HomeComponent,
-    LoginComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     SharedModule,
     AdminModule,
     ShoppingModule,
+    CoreModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
